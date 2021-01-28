bc77d3012c351c14191175606e3cb0c325bc823c
Ming <mingleiyou1988@gmail.com>
2021-01-28 13:38:30 +0000

    Add files via upload
    The Matlab implementation of the univariate Fox H function. 
    
    Please see the Example_Rayleigh_PDF.m for examples of using the functions.

---
+++ Example_Rayleigh_PDF.m
@@ -0,0 +1,49 @@
+% the test script calculates the PDF of the Rayleigh distribution with 0 mean
+% and unit variate in the form of univariate Fox H function and in its 
+% simplified representation.
+% The difference between the two calculations is one the level of 1e-14.
+%
+% For details regarding the univariate Fox H function and its related
+% issues, please refer to the book of "H Function: theory and
+% application"
+%
+% For details between other PDF/CDF/MGF of fading channels
+% and their Fox H representations, please refer to the paper and reference
+% therein.
+%
+% ****************************************************************************************************************
+% Citation: If you use this software or any (modified) part of it, please cite it as:
+% Minglei You, Hongjian Sun, Jing Jiang and Jiayi Zhang, ``Unified framework for
+% the effective rate analysis  % of wireless communication systems over MISO fading channels'',
+% IEEE  % Transactions on Communications, 65(4), pp. 1775-1785, April, 2017.
+% https://ieeexplore.ieee.org/abstract/document/7782422
+% https://arxiv.org/pdf/1612.03882.pdf
+% ****************************************************************************************************************
+% The complex gamma function credits to Paul Godfrey, see http://winnie.fit.edu/~gabdo/gamma.txt. 
+% ****************************************************************************************************************
+
+evaluation_sample_range = 0.1:0.1:3;
+Rayleigh_pdf_Fox_H = zeros(1, length(evaluation_sample_range));
+Rayleigh_pdf_SimpleFunction = Rayleigh_pdf_Fox_H;
+
+the_counter=0;
+for z=evaluation_sample_range
+    % syms z
+    the_counter=the_counter+1;
+    O=[1 0 0 1];
+    P={[1],[1],[],[0.5],[],[0.5]};
+    
+    
+    Rayleigh_pdf_Fox_H(the_counter)=func_Fox_H_univariate(z, O, P);
+
+    Rayleigh_pdf_SimpleFunction(the_counter)=2*z*exp(-z^2);
+end
+
+diff_result=Rayleigh_pdf_Fox_H-Rayleigh_pdf_SimpleFunction
+
+figure(1)
+plot(Rayleigh_pdf_Fox_H, '-*k')
+hold on
+plot(Rayleigh_pdf_SimpleFunction, '-or')
+
+
---
+++ func_Fox_H_univariate.m
@@ -0,0 +1,62 @@
+%% version 20200128 by Ming
+% this is a function to evaluate the univariate fox-h function using matlab
+% See definiation of the Fox_H in the book of "H Function: theory and
+% application"
+
+% ****************************************************************************************************************
+% Citation: If you use this software or any (modified) part of it, please cite it as:
+% Minglei You, Hongjian Sun, Jing Jiang and Jiayi Zhang, ``Unified framework for
+% the effective rate analysis  % of wireless communication systems over MISO fading channels'',
+% IEEE  % Transactions on Communications, 65(4), pp. 1775-1785, April, 2017.
+% https://ieeexplore.ieee.org/abstract/document/7782422
+% https://arxiv.org/pdf/1612.03882.pdf
+% ****************************************************************************************************************
+
+function Fox_H_value=func_Fox_H_univariate(z, O, P)
+
+% parameters defination here
+length=50;
+leave_from_real_axis=0.1;
+% disamble the O and P
+m=O(1);
+n=O(2);
+p=O(3);
+q=O(4);
+
+k=P{1};
+c=P{2};
+a=P{3};
+b=P{4};
+A=P{5};
+B=P{6};
+Fox_H_value_temp=real(integral(@(s)mnpq_part_generation(c*z,s,m,n,p,q,a,b,A,B), leave_from_real_axis-1i*length, leave_from_real_axis+1i*length,'RelTol', 1.e-7,'ArrayValued',true)/(2*pi*1i));
+Fox_H_value=Fox_H_value_temp*k;
+
+end
+
+function mnpq_part=mnpq_part_generation(z,s,m,n,p,q,a,b,A,B)
+% initialization for the integration theta(s)
+m_part= 1;
+n_part= 1;
+p_part= 1;
+q_part= 1;
+% create the integration theta(s)
+for iii=1:1:m
+    m_part= m_part.*gammaz(b(iii)-B(iii).*s);
+end
+
+for iii=1:1:n
+    n_part= n_part.*gammaz(1-a(iii)+A(iii).*s);
+end
+
+for iii=n+1:1:p
+    p_part= p_part.*gammaz(a(iii)-A(iii).*s);
+end
+
+for iii=m+1:1:q
+    q_part= q_part.*gammaz(1-b(iii)+B(iii).*s);
+end
+
+mnpq_part=z.^s.*m_part.*n_part./p_part./q_part;
+
+end
